221d639e60ba3c2ed0ef17c234c248e630d4e0dc
Elli Lehtimäki <elli.i.lehtimaki@gmail.com>
2025-11-24 15:56:29 +0200

    isEmpty: update tests to consider detailed e-commerce web app scenarios

--- tests/isEmpty.test.js
+++ tests/isEmpty.test.js
@@ -1,28 +1,22 @@
 import isEmpty from "../COMP.SE.200-2024-2025-1/src/isEmpty.js";
 
 describe("Tests for isEmpty", () => {
-    test("Null value", () => {
-        expect(isEmpty(null)).toBe(true)
+    test("The cart is empty", () => {
+        const cart = null
+        expect(isEmpty(cart)).toBe(true)
     });
 
-    test("True value", () => {
-        expect(isEmpty(true)).toBe(true)
+    test("The cart is empty", () => {
+        const cartIsEmpty = true
+        expect(isEmpty(cartIsEmpty)).toBe(true)
     });
 
-    test("Value is 1", () => {
-        expect(isEmpty(1)).toBe(true)
-    });
-
-    test("Value is a list with elements", () => {
-        expect(isEmpty([1, 2, 3])).toBe(false)
-    });
-
-    test("Value is a string", () => {
-        expect(isEmpty('abc')).toBe(false)
-    });
-
-    test("Value is a dictionary with elements", () => {
-        expect(isEmpty({ 'a': 1 })).toBe(false)
+    test("Cart has items", () => {
+        const cart = [
+            { productId: 1, quantity: 2 },
+            { productId: 2, quantity: 10 }
+        ];
+        expect(isEmpty(cart)).toBe(false)
     });
 
 })
