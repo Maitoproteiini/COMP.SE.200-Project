dc47787953e389a6b6fb3f7bd3ccec65e1441355
Elli Lehtimäki <elli.i.lehtimaki@gmail.com>
2025-11-24 14:39:18 +0200

    filter: update tests to be more useful for the e-commerce web app

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -1,22 +1,26 @@
 import filter from "../COMP.SE.200-2024-2025-1/src/filter.js";
 
 describe("Tests for filter", () => {
-    test("Return even numbers", () => {
-        const numbers = [1, 2, 3, 4, 5, 6, 7];
-        const result = filter(numbers, (value) => value % 2 === 0);
-        expect(result).toEqual([2, 4, 6]);
+    test("Return active users", () => {
+        const users = [
+            { user: 'barney', active: true },
+            { user: 'fred', active: false}
+        ];
+        const result = filter(users, ({ active }) => active);
+        expect(result).toEqual([{ user: 'barney', active: true }])
     });
 
-    test("Return odd numbers", () => {
-        const numbers = [1, 2, 3, 4, 5, 6, 7];
-        const result = filter(numbers, (value) => value % 2 !== 0);
-        expect(result).toEqual([1, 3, 5, 7]);
-    });
-
-    test("Return empty array of arrays if predicate is not satisfied", () => {
-        const numbers = [1, 2, 3, 4, 5, 6, 7];
-        const result = filter(numbers, (value) => value > 10);
-        expect(result).toEqual([[]]);
+    test("Returns in-stock products", () => {
+        const products = [
+            { product: 'banana', instock: true },
+            { product: 'apple', instock: false},
+            { product: 'milk', instock: true }
+        ];
+        const result = filter(products, ({ instock }) => instock);
+        expect(result).toEqual([
+            { product: 'banana', instock: true },
+            { product: 'milk', instock: true}
+        ])
     });
 
 })
