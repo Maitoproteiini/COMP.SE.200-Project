69846ace6bc3d6acb4aabc7838e47bbf3dc6b0a4
sqothi <otto-harald.hirvonen@tuni.fi>
2025-12-09 19:25:49 +0200

    Fixed jest coverage issue

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,8 +6,11 @@ module.exports = {
   // moduleNameMapper: {
   //   "^src/(.*)$": "<rootDir>/$1"
   // },
+  collectCoverageFrom: [
+    "COMP.SE.200-2024-2025-1/src/**/*.js"
+  ],
   collectCoverage: true,
-  coverageDirectory: "coverage",
+  coverageDirectory: "coverage/jest",
   coverageReporters: ["lcov", "text"],
   testPathIgnorePatterns: [
     "/node_modules/",
